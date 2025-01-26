37173a6b732559e0416012c53d6194cc9c492f74
saperico <sapijaszkoeryk@gmail.com>
2025-01-26 10:43:01 +0100

    Added login page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ClassPage from './ClassPage';
 import HomePage from './HomePage';
 import StudentPage from './StudentPage';
+import LoginPage from './LoginPage'; // Create a separate LoginPage component
 import './HomePage.css';
 import './ClassPage.css';
 import './StudentPage.css';
@@ -9,14 +10,24 @@ import './StudentsTable.css';
 import './editAbsenceLimit.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-
 function App() {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            setIsAuthenticated(true);
+        }
+    }, []);
 
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/class/:classNumber" element={<ClassPage/>} />
+                <Route
+                    path="/"
+                    element={isAuthenticated ? <HomePage /> : <LoginPage />}
+                />
+                <Route path="/class/:classNumber" element={<ClassPage />} />
                 <Route
                     path="/class/:classNumber/student/:studentNumber"
                     element={<StudentPage />}
